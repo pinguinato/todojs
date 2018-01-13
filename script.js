70d93b27f4ec3808a2a72d15f49cbc45dcfbe318
Roberto Gianotto <gianottoroberto@gmail.com>
2018-01-13 10:28:39 +0100

    Aggiunta di un todo

--- script.js
+++ script.js
@@ -34,29 +34,56 @@ document.addEventListener('DOMContentLoaded',function(){
 
   var doc = document; // il document
   // gli passo l'id della mia lista
-  var todoList = doc.querySelector('#todo');
+  var todoList = doc.querySelector('#todolist');
+  // seleziono l'input text
+  var todoInput = doc.querySelector('#todo');
+  // catturo l'evento tastiera
+  todoInput.addEventListener('keypress',function(e){
+    //console.dir(e); // mi restituisce in console il keycode
+    // controllo che sia stato inserito invio e che il valore non sia vuoto
+    
+    //console.log(e.keyCode);
+    //console.log(e.target.value);
+    // aggiungi il todo
+    if(e.keyCode === 13 && e.target.value && e.target.value.length >= 3){
+      myApp.addTodo(e.target.value);
+      // pulisco la casella di input
+      e.target.value = '';
+      console.dir(myApp.getTodos());
+    }
+
+    //console.log(e.keycode);
+    
+    /*
+
+    if(e.keycode === 13 && e.target.value && e.target.value.length >= 3){
+      console.log(e.target.value);
+      
+      console.dir(myApp.getTodos());
+    }
+
+    */
+
+  });
 
   var todos = myApp.getTodos(); // abbiamo un array di todos
 
-  var elementi = todos.map(function(todo){
-    return '<li>'+todo+'</li>';
-  });
+  //var elementi = todos.map(function(todo){
+  //  return '<li>'+todo+'</li>';
+  //});
 
-  // var li;
-  // var todoText;
-  //
-  // todos.forEach(function(todo){
-  //   //console.log(todo+'\n');
-  //   doc.createElement('li');
-  //   todoText = doc.createTextNode(todo);
+   var li, todoText;
+  
+   todos.forEach(function(todo){
+     console.log(todo+'\n');
+     li = doc.createElement('li');
+     todoText = doc.createTextNode(todo);
   //   //console.log(todoText);
-  //
-  //   li.appendchild(todoText);
-  //
-  //   todoList.appendchild(li);
-  // });
+     li.appendChild(todoText);
+     todoList.appendChild(li);
+   });
+  
 
-
-  todoList.innerHTML = elementi;
+  //todoList.innerHTML = elementi;
 
 });
